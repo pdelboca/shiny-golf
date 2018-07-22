03f1cb39eae3db0b800b992466a6c3c0096f2598
pdelboca <patriciodelboca@gmail.com>
2018-07-22 13:37:37 -0300

    Function to get card data from GoogleSheets

--- data/golf_data.R
+++ data/golf_data.R
@@ -1,4 +1,17 @@
 # Script to load data from GoogleSheet
 library(googlesheets)
+library(dplyr)
+library(tidyr)
+library(lubridate)
 
-gs_auth()
+get_cards <- function(){
+  gs_title("datos_golf") %>%
+    gs_read("Tarjetas") %>%
+    select("Fecha":"18") %>%
+    gather("Hole", "Shots", "1":"18") %>%
+    mutate(Fecha = dmy(Fecha),
+           Hole = as.numeric(Hole)) %>%
+    arrange(Fecha, Cancha, Hole)
+}
+
+cards <- get_cards()
